4a1fc2c105cedd2b0200594728c9d95d7efdfd0c
hao.huang-home <huanghao870620@163.com>
2017-07-02 15:56:30 +0800

    commit

--- a10.asm
+++ a10.asm
@@ -1,1 +1,34 @@
 .386
+.model flat, stdcall
+option casemap: none
+
+include windows.inc
+include kernel32.inc
+include user32.inc
+include gdi32.inc
+
+includelib kernel32.lib
+includelib user32.lib
+includelib gdi32.lib
+
+.data
+
+
+.code
+
+main PROC
+  
+  
+main endp
+
+a0 proc
+a0 endp
+
+a1 proc
+a1 endp
+
+
+
+end main
+
+
