2130795c39b66919f072f493a21187a4c8cb8b83
burgess <huanghao870620@163.com>
2017-07-06 16:58:38 +0800

    commit

---
+++ a14.asm
@@ -0,0 +1,21 @@
+.386
+.model flat, stdcall
+option casemap: none
+
+include windows.inc
+include user32.inc
+include kernel32.inc
+
+includelib user32.lib
+includelib kernel32.lib
+
+.data
+
+.code
+
+
+main proc
+
+main endp
+
+end main
